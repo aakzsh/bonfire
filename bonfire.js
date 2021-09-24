80a924e3fa7804e5ea08521334dcab945f1f8a5f
shruti <shrutigupta5555@gmail.com>
2021-09-25 01:43:16 +0530

    do weird hologram shiz

--- bonfire.js
+++ bonfire.js
@@ -1,15 +1,45 @@
 const ytSection = document.querySelector('.yt-video')
-
+const playBtn = document.querySelector('.play-btn')
 
 window.addEventListener('load', (e) => {
     const id = window.location.href.split("=")[1];
-    console.log(ytSection)
+    // console.log(ytSection)
 
     ytSection.innerHTML += `
-        <div class="1">
-            <embed width="315" height="315"
+
+            <iframe class="a"
             src="https://www.youtube.com/embed/${id}">
-            </embed> 
-        </div>
+            </iframe> 
+            <div class="flex-em">
+              <iframe class="b"
+                src="https://www.youtube.com/embed/${id}">
+              </iframe> 
+              <div class="space"></div>
+              <iframe class="c"
+                src="https://www.youtube.com/embed/${id}">
+              </iframe> 
+            </div>
+            
+            <iframe class="d"
+            src="https://www.youtube.com/embed/${id}">
+            </iframe> 
+       
+       
     `
+
+    //control autoplay
+    const a = document.querySelector('.a');
+    const b = document.querySelector('.b');
+    const c = document.querySelector('.c');
+    const d = document.querySelector('.d');
+
+    playBtn.addEventListener('click', e => {
+      e.preventDefault()
+      a.src += '?autoplay=1'
+      console.log(a.src)
+    })
+
+
 })
+
+
